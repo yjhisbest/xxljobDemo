d6863d1a9abe007629043ca209bd7924eed5e838
yjhisbest <1411425707@qq.com>
2019-05-27 01:24:28 +0800

    add hello

--- src/main/java/com/yjh/controller/HelloController.java
+++ src/main/java/com/yjh/controller/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hello")
+@RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/")
+    @RequestMapping("/hello")
     public String hello(){
         return "Hello Jkenis!!";
     }
